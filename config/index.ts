7911f957333093d539bf5562b6441efdedc6af83
hariharan7089 <hariharank007089@gmail.com>
2025-03-22 08:52:38 +0530

    new Project v1 Update

---
+++ config/index.ts
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "MeetUpIn",
+  description: "Modern Next.js powered Video calling app",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "getstream",
+    "stream",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "MeetUpIn",
+    "realtime-video",
+    "realtime-chat",
+    "live-audio",
+    "live-video",
+    "live-chat",
+    "radix-toast",
+    "lucide-react",
+    "next-themes",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "ts",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "hariharan k",
+    url: "https://github.com/hariharan7089",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/hariharan7089/MeetUpIn",
+} as const;
